51ac093745bcb257edcb9377e9bb0ef0e07759b2
EsleySaab <esleysantana.sa@gmail.com>
2025-01-14 14:47:15 -0300

    feat: create a transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles"
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              <td>Venda</td>
+              <td>14/01/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              <td>Alimentação</td>
+              <td>13/01/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
